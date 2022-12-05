786fb44fcfeef202a0a4ea1cabe617e8ffa5e01b
tmutas <tmutas@users.noreply.github.com>
2022-12-05 22:51:38 +0100

    Day 04 Part 2

--- aoc/04/Main.scala
+++ aoc/04/Main.scala
@@ -4,20 +4,27 @@ import scala.io.Source._
     val lines : Iterator[String] = fromFile(args(0)).getLines()
     
     var score = 0
+    var score2 = 0
     for 
         line <- lines
     do
         val ranges = for v <- line.split(",") yield v.split("-").map(x => x.toInt)
         val diffs = for v <- ranges yield v(1) - v(0)
+        // Part 1
         if 
-            diffs(0) >= diffs(1)            
+            diffs(0) >= diffs(1)
         then
             if ranges(0)(0) <= ranges(1)(0) && ranges(0)(1) >= ranges(1)(1) then score += 1
         else 
             if ranges(0)(0) >= ranges(1)(0) && ranges(0)(1) <= ranges(1)(1) then score += 1
-
+        // Part 2
+        if 
+            (ranges(0)(0) <= ranges(1)(0) && ranges(0)(1) >= ranges(1)(0)) ||
+            (ranges(1)(0) <= ranges(0)(0) && ranges(1)(1) >= ranges(0)(0))
+        then score2 += 1
 
     println(s"Part 1: $score")
+    println(s"Part 2: $score2")
 
 
         
