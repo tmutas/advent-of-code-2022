aa01f1b75707c49eb3ce9a3ac3c7dece407bcea2
tmutas <tmutas@users.noreply.github.com>
2022-12-10 11:19:33 +0100

    Day 09 Part 2

--- aoc/09/Main.scala
+++ aoc/09/Main.scala
@@ -41,6 +41,25 @@ class Rope(val head : Point = Point(), val tail : Point = Point()):
         // Returns tail position after every step
         for i <- 1 to steps do tailPositions += performSingleStep(direction)
 
+class LongRope(val numKnots : Int = 10):
+    val tailPositions = mutable.Set[Point]()
+
+    val knots = for i <- 0 until numKnots yield Point()
+
+    def performSingleStep(direction : String) : Point = 
+        knots(0).moveFromInstruction(direction)
+        for 
+            i <- 1 until knots.size
+        do
+            val diff = knots(i).diff(knots(i-1))
+            if diff.maxAbs >= 2 then knots(i).moveFromDiff(diff)
+        
+        return knots.last.copy()
+
+    def performSteps(direction : String, steps : Int) =
+        // Returns tail position after every step
+        for i <- 1 to steps do tailPositions += performSingleStep(direction)
+
 @main def Run(args: String*) =
     val lines : Iterator[String] = 
         if args.nonEmpty then fromFile(args(0)).getLines() else Array[String]().iterator
@@ -50,8 +69,14 @@ class Rope(val head : Point = Point(), val tail : Point = Point()):
 
     var rope = Rope()
     
-    val instructions = for line <- lines yield line.split(" ")
+    val instructions = (for line <- lines yield line.split(" ")).duplicate
 
-    for inst <- instructions do rope.performSteps(inst(0), inst(1).toInt) 
+    for inst <- instructions(0) do rope.performSteps(inst(0), inst(1).toInt) 
     
-    println(rope.tailPositions.size)
+    println(s"Part 1 : ${rope.tailPositions.size}")
+
+    var rope2 = LongRope()
+
+    for inst <- instructions(1) do rope2.performSteps(inst(0), inst(1).toInt) 
+
+    println(s"Part 2 : ${rope2.tailPositions.size}")
